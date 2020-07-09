20ad4f00103b8ad55f7b6e2f0d5a7ffab5a2a031
Vitoria Oliveira <57400348+vitoriareges@users.noreply.github.com>
2020-07-09 15:15:17 +0100

    update pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,14 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'menu',
+    loadChildren: () => import('./app/menu/menu.module').then( m => m.MenuPageModule)
+  },
+  {
+    path: 'cart',
+    loadChildren: () => import('./app/cart/cart.module').then( m => m.CartPageModule)
   }
 ];
 
